545d06e73973ce7a97bb57cd78d5f039a8d14379
Erick Armingol <earmingol14@gmail.com>
2020-05-25 16:49:42 -0700

    Minor change

--- src/result_processing.py
+++ src/result_processing.py
@@ -2,15 +2,15 @@ import pandas as pd
 import numpy as np
 import src
 
-def extract_pagerank_results(result_dict, perturbed_prots):
-    perturbation_df = pd.DataFrame(columns=['Perturbation'] + perturbed_prots)
+def extract_pagerank_results(result_dict, prots_of_interest):
+    perturbation_df = pd.DataFrame(columns=['Perturbation'] + prots_of_interest)
 
     perturbations = set(result_dict.keys())
     perturbations.remove('Base-PageRank')
 
     perturbation_df['Perturbation'] = list(result_dict.keys())
 
-    for p in perturbed_prots:
+    for p in prots_of_interest:
         perturbation_df[p] = perturbation_df['Perturbation'].apply(
             lambda x: src.pagerank.div_pagerank(result_dict[x][p],
                                                 result_dict['Base-PageRank'][p]
